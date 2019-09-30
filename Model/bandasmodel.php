31c8d1dc08fe1d283d72725d1d351428f54eddea
Jeremias Caballero <jeremiascaballerocelan@gmail.com>
2019-09-30 20:20:49 -0300

    Agregue modelo

---
+++ Model/bandasmodel.php
@@ -0,0 +1,21 @@
+<?php
+class BandasModel
+{
+    function __construct(){
+        $this->db = $this->Connect();
+    }
+    private function connect(){
+        return new PDO('mysql:host=localhost;'.'dbname=bandas;charset=utf8','root', '');
+     }
+    function GetBandas(){
+        $sentencia = $this->db->prepare( "select * from banda");
+        $sentencia->execute();
+        return $sentencia->fetchAll(PDO::FETCH_OBJ);
+
+    }
+    
+}
+
+
+
+?>
