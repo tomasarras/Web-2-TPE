748147fe2237d8626deaa781b04375a7dbe9c55e
Jeremias Caballero <jeremiascaballerocelan@gmail.com>
2019-09-30 20:07:36 -0300

    modificacion de base url

--- route.php
+++ route.php
@@ -1,16 +1,26 @@
 <?php
 
-require_once "index.php";
-require_once "tareas.php";
+require_once "Controllers/bandascontroller.php";
 
+$action = $_GET["action"];
+define("BASE_URL", 'http://'.$_SERVER["SERVER_NAME"].':'.$_SERVER["SERVER_PORT"].dirname($_SERVER["PHP_SELF"]).'/');
+$controller = new BandasController();
 
-$partesUrl = explode('/', $_GET ['action']);
-if($partesUrl [0] == ''){
-    home();
+if($action == ''){
+    $controller->Home(); 
 }else{
-    if($partesUrl [0]  == 'agregar'){
-        insertTareas();
-}elseif($partesUrl[0] == 'borrar') {
-    Borrartares($partesUrl[1]);
+    if (isset($action)){
+        $partesURL = explode("/", $action);
+
+        if($partesURL[0] == "tareas"){
+            $controller->GetTareas();
+        }elseif($partesURL[0] == "insertar") {
+            $controller->InsertarTarea();
+        }elseif($partesURL[0] == "finalizar") {
+            $controller->FinalizarTarea($partesURL[1]);
+        }elseif($partesURL[0] == "borrar") {
+            $controller->BorrarTarea($partesURL[1]);
+        }
+    }
 }
-}
+
